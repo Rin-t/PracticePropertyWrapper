1b7a4e39100509bd2f05301cbb8577304f859fe6
Rin-t <rin.t.4921@gmail.com>
2023-04-23 21:37:28 +0900

    Initial Commit

---
+++ PracticePropertyWrapper/PracticePropertyWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticePropertyWrapperApp.swift
+//  PracticePropertyWrapper
+//
+//  Created by Rin on 2023/04/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticePropertyWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
